08d4341e0708f19acd786c2fb91e7ba74ec028da
Alex Kostas <k.alexandros299@gmail.com>
2019-06-20 00:19:35 +0300

    Add integer to roman number converter

--- RomanNumberConverter.UnitTests/RomanNumberConverterTests.cs
+++ RomanNumberConverter.UnitTests/RomanNumberConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace RomanNumberConverter.UnitTests {
     [TestFixture]
@@ -13,11 +14,31 @@ namespace RomanNumberConverter.UnitTests {
         [Test]
         [TestCase("X V I I")]
         [TestCase("XV II")]
+        [TestCase("DJSBVJD")]
+        [TestCase("xvii")]
         [TestCase(null)]
         [TestCase("    ")]
         [TestCase("")]
         public void RomanToInteger_GivenInvalidInput_ReturnsArgumentException(string input) {
             Assert.That(() => RomanNumberConverter.RomanToInteger(input), Throws.ArgumentException);
+        }
+
+        [Test]
+        [TestCase(19, "XIX")]
+        [TestCase(999, "CMXCIX")]
+        [TestCase(11984, "MMMMMMMMMMMCMLXXXIV")]
+        public void IntegerToRoman_GivenIntegers_ReturnsRomanNumber(int num, string expectedRoman) {
+            Assert.That(RomanNumberConverter.IntegerToRoman(num), Is.EqualTo(expectedRoman));
+        }
+
+        [Test]
+        [TestCase(-5768)]
+        [TestCase(-437686376)]
+        [TestCase(-10000)]
+        [TestCase(-1)]
+        public void IntegerToRoma_GivenInvalidInput_ReturnsArgumentOutOfRangeException(int input) {
+            Assert.That(() => RomanNumberConverter.IntegerToRoman(input),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
         }
     }
 }
--- RomanNumberConverter/RomanNumberConverter.cs
+++ RomanNumberConverter/RomanNumberConverter.cs
@@ -24,5 +24,26 @@ namespace RomanNumberConverter {
                     throw new ArgumentException();
             }
         }
+
+        //TODO: Change this to iterative solution
+        public static string IntegerToRoman(int number) {
+            if(number < 0)
+                throw new ArgumentOutOfRangeException();
+            if (number == 0) return "";
+            if (number >= 1000) return "M" + IntegerToRoman(number - 1000);
+            if (number >= 900) return "CM" + IntegerToRoman(number - 900); 
+            if (number >= 500) return "D" + IntegerToRoman(number - 500);
+            if (number >= 400) return "CD" + IntegerToRoman(number - 400);
+            if (number >= 100) return "C" + IntegerToRoman(number - 100);            
+            if (number >= 90) return "XC" + IntegerToRoman(number - 90);
+            if (number >= 50) return "L" + IntegerToRoman(number - 50);
+            if (number >= 40) return "XL" + IntegerToRoman(number - 40);
+            if (number >= 10) return "X" + IntegerToRoman(number - 10);
+            if (number >= 9) return "IX" + IntegerToRoman(number - 9);
+            if (number >= 5) return "V" + IntegerToRoman(number - 5);
+            if (number >= 4) return "IV" + IntegerToRoman(number - 4);
+            if (number >= 1) return "I" + IntegerToRoman(number - 1);
+            throw new Exception("Operation failed");
+        }
     }
 }
