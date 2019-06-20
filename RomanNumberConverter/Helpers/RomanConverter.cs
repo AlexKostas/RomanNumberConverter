cc38de6c3247240a82d4a3758e822a5318b40aa6
Alex Kostas <k.alexandros299@gmail.com>
2019-06-20 20:11:09 +0300

    Changed int to roman algorithm to iterative

--- RomanNumberConverter/Helpers/RomanConverter.cs
+++ RomanNumberConverter/Helpers/RomanConverter.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Linq;
+using System.Text;
 
 namespace RomanNumberConverter.Helpers {
     public static class RomanConverter {
         public static int RomanToInteger(string romanNumber) {
             if (string.IsNullOrWhiteSpace(romanNumber))
                 throw new ArgumentException();
-            
+
             var letters = romanNumber.ToCharArray();
             return letters.Sum(RomanLetterValue);
         }
@@ -20,30 +21,28 @@ namespace RomanNumberConverter.Helpers {
                 case 'C': return 100;
                 case 'D': return 500;
                 case 'M': return 1000;
-                default: 
+                default:
                     throw new ArgumentException();
             }
         }
 
-        //TODO: Change this to iterative solution
         public static string IntegerToRoman(int number) {
-            if(number < 0)
-                throw new ArgumentOutOfRangeException();
-            if (number == 0) return "";
-            if (number >= 1000) return "M" + IntegerToRoman(number - 1000);
-            if (number >= 900) return "CM" + IntegerToRoman(number - 900); 
-            if (number >= 500) return "D" + IntegerToRoman(number - 500);
-            if (number >= 400) return "CD" + IntegerToRoman(number - 400);
-            if (number >= 100) return "C" + IntegerToRoman(number - 100);            
-            if (number >= 90) return "XC" + IntegerToRoman(number - 90);
-            if (number >= 50) return "L" + IntegerToRoman(number - 50);
-            if (number >= 40) return "XL" + IntegerToRoman(number - 40);
-            if (number >= 10) return "X" + IntegerToRoman(number - 10);
-            if (number >= 9) return "IX" + IntegerToRoman(number - 9);
-            if (number >= 5) return "V" + IntegerToRoman(number - 5);
-            if (number >= 4) return "IV" + IntegerToRoman(number - 4);
-            if (number >= 1) return "I" + IntegerToRoman(number - 1);
-            throw new Exception("Operation failed");
+            if(number < 0) throw new ArgumentOutOfRangeException();
+            var result = new StringBuilder();
+            int[] digitsValues = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+            string[] romanDigits = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", 
+                "CM", "M" };
+            while (number > 0)
+            {
+                for (int i = digitsValues.Count() - 1; i >= 0; i--)
+                    if (number / digitsValues[i] >= 1)
+                    {
+                        number -= digitsValues[i];
+                        result.Append(romanDigits[i]);
+                        break;
+                    }
+            }
+            return result.ToString();
         }
     }
 }
